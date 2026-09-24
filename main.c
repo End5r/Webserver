338a2c91737d6ac352d8b7c872809b27b16416f7
End5r <ehneudel2@gmail.com>
2026-09-24 12:59:39 +0200

    Server-Schleife verbessert

--- main.c
+++ main.c
@@ -12,21 +12,48 @@ int main(){
    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
    bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)); // Socket wird mit der ServerAdresse verbunden
-   printf("bind successfull");
+   //printf("bind successfull\n");
 
    listen(serverSocket, 5); // maximale Nummer an Requests in einer Schlange 
-   printf("Server listening on Port: %d\n", 8080);
-
-   char response[]=
-   "HTTP/1.1 200 OK \r\n"
-   "Content-Type: text/plain\r\n"
-   "Content-Length: 12\r\n"
-   "\r\n"
-   "Hello World\n";
+   //printf("Server listening on Port: %d\n", 8080);
 
    while(1) {
 	   int clientSocket = accept(serverSocket, NULL, NULL); //Verbindung zwischen Server und Client annehmen
-	   write(clientSocket, response, strlen(response));	//Antwort
+	   
+	   char recBuf[1024] = {0};	//HTTP Request Platzhalter
+	   recv(clientSocket, recBuf, 1024, 0); // Request wird "recieved"
+	   //printf("%s\n", recBuf);
+
+	   char *position = recBuf + 5; //Startpunkt des Paths
+	   //printf("%s\n", position);
+	   char *path = strtok(position, " ");	//Path wird extrahiert
+	   //printf("%s\n", path);
+
+	   const char *file;	// Compare HTTP Request
+	   if (strcmp(path, "index.html") == 0) {
+		   file = "./index.html";
+	   } else {
+		   file = "./notFound.html";
+	   }
+
+	   FILE *html = fopen(file, "r");	//Öffne und Lese aus der html
+	
+	   char buf[1024] = {0};	// Platzhalter
+	   size_t read = 0;			// Anzahl an übertragenen Bytes
+
+	   char *header	=		// HTTP RESPONSE
+	   "HTTP/1.1 200 OK\r\n"
+	   "Content-Type: text/html\r\n\r\n";
+	   
+	   send(clientSocket,header,strlen(header),0);	//Sende an den Client den HTTP Response
+	   
+	   while((read = fread(buf, sizeof(buf[0]), 1024, html)) > 0){ // Sende schrittweise die HTML
+		   send(clientSocket, buf, read, 0);
+		   }
+	   
+	   fclose(html);	// File wird wieder geschlossen
+	   close(clientSocket);
+	   //printf("disconnected client\n");
    }
 
    close(serverSocket);
