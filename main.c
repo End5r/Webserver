58639f456b1b1d2353330eaec00845d44ef1494a
End5r <ehneudel2@gmail.com>
2026-09-26 17:03:02 +0200

    fixed Not Found

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <netinet/in.h>
 #include <unistd.h> // close()
 #include <string.h> // strlen
-int main(){
+int main(){	
    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);  //Endpunkt wird erstellt
 
    struct sockaddr_in serverAddress;    // Socket Adresse von dem Server
@@ -21,6 +21,7 @@ int main(){
    //printf("Server listening on Port: %d\n", 8080);
 
    while(1) {
+	   int status = 0;
 	   int clientSocket;
 	   if ((clientSocket = accept(serverSocket, NULL, NULL)) < 0){
 		perror("accept failed");
@@ -31,31 +32,38 @@ int main(){
 	   recv(clientSocket, recBuf, 1024, 0); // Request wird "recieved"
 	   //printf("%s\n", recBuf);
 
-	   char *position = recBuf + 5; //Startpunkt des Paths
+	   char *position = recBuf + 4; //Startpunkt des Paths
 	   //printf("%s\n", position);
 	   char *path = strtok(position, " ");	//Path wird extrahiert
-	   //printf("%s\n", path);
+	   printf("%s\n", path);
 
 	   const char *file;	// Compare HTTP Request
-	   if (strcmp(path, "index.html") == 0) {
+	   if (strcmp(path, "/index.html") == 0 || strcmp(path, "/") == 0) {
 		   file = "./index.html";
 	   } else {
 		   file = "./notFound.html";
-	   }
+		   status = -1;
+		}
 
 	   FILE *html;
-	   if ((html = fopen(file, "r")) < 0){
+	   if ((html = fopen(file, "r")) == NULL){
 		perror("couldnt open file");
-		return -1;
+		close(clientSocket);
 	   }	//Öffne und Lese aus der html
 	
 	   char buf[1024] = {0};	// Platzhalter
 	   size_t read = 0;			// Anzahl an übertragenen Bytes
 
-	   char *header	=		// HTTP RESPONSE
-	   "HTTP/1.1 200 OK\r\n"
-	   "Content-Type: text/html\r\n\r\n";
-	   
+	   char *header;
+	   if (status == -1) {
+			header =
+			"HTTP/1.1 404 Not Found\r\n"
+			"Content-Type: text/html\r\n\r\n";
+	   } else {
+	   		header	=		// HTTP RESPONSE
+	   		"HTTP/1.1 200 OK\r\n"
+	   		"Content-Type: text/html\r\n\r\n";
+	   }
 	   send(clientSocket,header,strlen(header),0);	//Sende an den Client den HTTP Response
 	   
 	   while((read = fread(buf, sizeof(buf[0]), 1024, html)) > 0){ // Sende schrittweise die HTML
